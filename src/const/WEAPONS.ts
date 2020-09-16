cc91c0c3024890fafa759ac5716d044d6275b32a
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2020-09-17 01:37:30 +0200

    Deploy live med Vercel

--- src/const/WEAPONS.ts
+++ src/const/WEAPONS.ts
@@ -1,17 +1,14 @@
 const WEAPONS = [
   {
     name: 'Stein',
-    emoji: '✊',
     beats: 'Saks',
   },
   {
     name: 'Papir',
-    emoji: '✋',
     beats: 'Stein',
   },
   {
     name: 'Saks',
-    emoji: '✌',
     beats: 'Papir',
   },
 ];
