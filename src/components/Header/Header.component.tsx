7ab73490d4251c83d249a7d03fb09e73f0379e5c
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2020-09-16 23:09:05 +0200

    Legg til introtekst og våpen
    Vi trengte å legge til introtekst med forklaring. Vi la også til våpen i en separat komponent med SVG.

---
+++ src/components/Header/Header.component.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Navbar from 'react-bootstrap/Navbar';
+
+function Header() {
+  return (
+    <Navbar bg="dark" variant="dark">
+      <h2 className="mx-auto text-white">Stein Saks Papir</h2>
+    </Navbar>
+  );
+}
+
+export default Header;
