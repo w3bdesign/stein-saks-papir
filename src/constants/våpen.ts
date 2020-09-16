2d257f0ec5043c21fc94b5870c4b1663c7607c83
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2020-09-16 20:16:08 +0200

    Setting up React-bootstrap for layout and design
    Also added a project description

---
+++ src/constants/våpen.ts
@@ -0,0 +1,19 @@
+const VÅPEN = [
+  {
+    name: 'Stein',
+    emoji: '✊',
+    beats: 'Saks',
+  },
+  {
+    name: 'Papir',
+    emoji: '✋',
+    beats: 'Stein',
+  },
+  {
+    name: 'Saks',
+    emoji: '✌',
+    beats: 'Papir',
+  },
+];
+
+export default VÅPEN;
