ab76d90fba6880f1667706b10e8dc964887bcd18
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2020-09-17 03:53:14 +0200

    Satt opp Context og useContext
    Vi henter nå både navn og score fra Context

---
+++ src/context/ScoreBoardContext.ts
@@ -0,0 +1,7 @@
+import { createContext } from 'react';
+
+import DEFAULT_SCORE from '../const/DEFAULT_SCORE';
+
+const StoreBoardContext = createContext(DEFAULT_SCORE);
+
+export default StoreBoardContext;
--- src/index.tsx
+++ src/index.tsx
@@ -4,12 +4,18 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import Header from './components/Layout/Header/Header.component';
 
+import DEFAULT_SCORE from './const/DEFAULT_SCORE';
+
+import ScoreBoardContext from './context/ScoreBoardContext';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <>
-    <Header />
-    <App />
+    <ScoreBoardContext.Provider value={DEFAULT_SCORE}>
+      <Header />
+      <App />
+    </ScoreBoardContext.Provider>
   </>,
   document.getElementById('root'),
 );
