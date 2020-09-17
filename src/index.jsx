c89a15465d068470d6c890f33143823e060d294c
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2020-09-17 04:48:07 +0200

    Begynn å implementere mer funksjonalitet
    Videre jobb med funksjonalitet

--- src/index.jsx
+++ src/index.jsx
@@ -4,20 +4,14 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import Header from './components/Layout/Header/Header.component';
 
-import DEFAULT_SCORE from './const/DEFAULT_SCORE';
-
-import ScoreBoardContext from './context/ScoreBoardContext';
-
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/stylesheet.css';
 import './css/animate.min.css';
 
 ReactDOM.render(
   <>
-    <ScoreBoardContext.Provider value={DEFAULT_SCORE}>
-      <Header />
-      <App />
-    </ScoreBoardContext.Provider>
+    <Header />
+    <App />
   </>,
   document.getElementById('root'),
 );
