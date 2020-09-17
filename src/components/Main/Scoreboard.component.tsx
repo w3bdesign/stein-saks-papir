5436a3f9fc7b647a8ac9bf6374ccfe52b6e70e54
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2020-09-17 02:15:06 +0200

    Ferdig med poengtavle design
    Vi er ferdig med design av poengtavle og resett knapp

--- src/components/Main/Scoreboard.component.tsx
+++ src/components/Main/Scoreboard.component.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card } from 'react-bootstrap';
+import { Card, CardDeck } from 'react-bootstrap';
 
 function Scoreboard() {
   return (
@@ -8,7 +8,27 @@ function Scoreboard() {
         <Card.Header>
           <h5>Poengtavle</h5>
         </Card.Header>
-        <Card.Body>Spiller | CPU</Card.Body>
+
+        <CardDeck className="m-4">
+          <Card>
+            <Card.Body>
+              <Card.Title><h5>Spiller 1</h5></Card.Title>
+              <Card.Text>
+                <h3>0</h3>
+              </Card.Text>
+            </Card.Body>
+
+          </Card>
+          <Card>
+            <Card.Body>
+              <Card.Title><h5>CPU</h5></Card.Title>
+              <Card.Text>
+                <h3>0</h3>
+              </Card.Text>
+            </Card.Body>
+
+          </Card>
+        </CardDeck>
       </Card>
     </>
   );
