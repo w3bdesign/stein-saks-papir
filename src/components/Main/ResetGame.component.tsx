c3f13f45d220e00555c6dab5b7571042bfa36f17
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2020-09-17 02:15:06 +0200

    Ferdig med poengtavle design
    Vi er ferdig med design av poengtavle og resett knapp

---
+++ src/components/Main/ResetGame.component.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Button } from 'react-bootstrap';
+
+function ResetGame() {
+  return (
+    <>
+      <Button variant="danger" className="mt-0 mb-4">
+        Restart spill
+      </Button>
+    </>
+  );
+}
+
+export default ResetGame;
