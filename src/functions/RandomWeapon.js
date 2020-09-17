529696eb56bb0305d91434f8f2dba10dca03c103
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2020-09-17 23:39:59 +0200

    Vi kan nå gjøre tilfeldig valg av våpen

---
+++ src/functions/RandomWeapon.js
@@ -0,0 +1,8 @@
+import WEAPONS from '../const/WEAPONS';
+
+function RandomWeapon() {
+  const randomIndex = Math.floor(Math.random() * WEAPONS.length);
+  return WEAPONS[randomIndex];
+}
+
+export default RandomWeapon;
