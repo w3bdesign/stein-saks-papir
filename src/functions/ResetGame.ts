2fe53bc1336c551246f69927f7793288b4b7ba1b
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2020-09-17 04:54:03 +0200

    Fiks Typescript error

--- src/functions/ResetGame.ts
+++ src/functions/ResetGame.ts
@@ -3,8 +3,8 @@ import { useContext } from 'react';
 import ScoreBoardContext from '../context/ScoreBoardContext';
 
 function ResetGame() {
-  const [score, setScore] = useContext(ScoreBoardContext);
-  console.log('Reset game ...');
+  // const [score, setScore] = useContext(ScoreBoardContext);
+  // console.log('Reset game ...');
   // setScore(null);
   // console.log(Weapon);
 }
