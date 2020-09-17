2d58f4ea2d63c9b5b0d196a7a1d5076e2d51cd95
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2020-09-17 16:14:49 +0200

    Fiks warnings så app kan deploye
    Fjernet console.log

--- src/functions/PlayGame.ts
+++ src/functions/PlayGame.ts
@@ -1,8 +1,9 @@
 // import WEAPONS from '../const/WEAPONS';
 
-function PlayGame(Weapon: string) {
-  console.log('Playing game ...');
-  console.log(Weapon);
+// function PlayGame(Weapon: string) {
+function PlayGame() {
+  // console.log('Playing game ...');
+  // console.log(Weapon);
 }
 
 export default PlayGame;
