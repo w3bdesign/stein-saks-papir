50613b42f29922a99ea5323a8cf0c151f77e66df
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2020-09-17 02:36:46 +0200

    Restrukturering av mapper og innhold
    Separering av mapper til Layout og Pages.
    
    Bør gjøre ting litt ryddigere.

---
+++ src/components/Layout/Footer/Footer.component.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Navbar from 'react-bootstrap/Navbar';
+
+function Footer() {
+  return (
+    <Navbar bg="dark" variant="dark">
+      <h3 className="mx-auto text-white">Copyright (c) Daniel</h3>
+    </Navbar>
+  );
+}
+
+export default Footer;
