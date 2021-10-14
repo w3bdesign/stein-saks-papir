8836f2037b7e1301ce91b2d63c564377a74f0afb
Daniel Fjeldstad <daniel.fjeldstad@novacare.no>
2021-10-14 04:16:13 +0200

    Sorter import alfabetisk

--- src/__tests__/ElementsVisible.test.jsx
+++ src/__tests__/ElementsVisible.test.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import { render } from '@testing-library/react';
-import { describe, test, expect } from '@jest/globals';
+import { describe, expect, test } from '@jest/globals';
 
 import App from '../App';
 
