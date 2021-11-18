5ae3d05bfe1f0ca2ec0fa1379c348c40cb8cd98f
Daniel Fjeldstad <daniel.fjeldstad@novacare.no>
2021-11-18 02:24:10 +0100

    Remove unused code

--- src/state/model/index.js
+++
@@ -1,9 +0,0 @@
-import gameModel from './gameModel';
-import scoreModel from './scoreModel';
-
-const gameScoreModel = {
-  game: gameModel,
-  score: scoreModel,
-};
-
-export default gameScoreModel;
