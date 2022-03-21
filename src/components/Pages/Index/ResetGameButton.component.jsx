a78a1e33e080af4916bc3fbf509325a89edee72c
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2022-03-21 20:23:43 +0100

    Skru av Redux devtools i produksjon
    https://github.com/ctrlplusb/easy-peasy/issues/548

--- src/components/Pages/Index/ResetGameButton.component.jsx
+++ src/components/Pages/Index/ResetGameButton.component.jsx
@@ -4,11 +4,11 @@ import { useStoreActions } from 'easy-peasy';
 import Button from 'react-bootstrap/Button';
 
 function ResetGameButton() {
-  const increaseScore = useStoreActions((actions) => actions.reset);
+  const resetGame = useStoreActions((actions) => actions.reset);
 
   return (
     <>
-      <Button variant="danger" className="mt-0 mb-4" onClick={increaseScore}>
+      <Button variant="danger" className="mt-0 mb-4" onClick={resetGame}>
         Restart spill
       </Button>
     </>
