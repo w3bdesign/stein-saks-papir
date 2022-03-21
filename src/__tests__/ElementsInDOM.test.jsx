d2b8859359d2122bfbdfe6f31cd8254a50ef582a
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2022-03-21 20:31:19 +0100

    Npm lint

--- src/__tests__/ElementsInDOM.test.jsx
+++ src/__tests__/ElementsInDOM.test.jsx
@@ -1,45 +1,45 @@
-import React from "react";
+import React from 'react';
 
-import { render } from "@testing-library/react";
-import { describe, expect, test } from "@jest/globals";
+import { render } from '@testing-library/react';
+import { describe, expect, test } from '@jest/globals';
 
-import App from "../App";
-import Header from "../components/Layout/Header/Header.component";
+import App from '../App';
+import Header from '../components/Layout/Header/Header.component';
 
-describe("Sjekk at alle nødvendige elementer er på plass i dokumentet", () => {
-  test("Sjekk at navbar er på plass", () => {
+describe('Sjekk at alle nødvendige elementer er på plass i dokumentet', () => {
+  test('Sjekk at navbar er på plass', () => {
     const { getByRole } = render(<Header />);
-    const Navbar = getByRole("heading", { name: /Stein Saks Papir/i });
+    const Navbar = getByRole('heading', { name: /Stein Saks Papir/i });
     expect(Navbar).toBeInTheDocument();
   });
 
-  test("Sjekk at saks er på plass", () => {
+  test('Sjekk at saks er på plass', () => {
     const { getByRole } = render(<App />);
-    const Saks = getByRole("button", { name: /saks/i });
+    const Saks = getByRole('button', { name: /saks/i });
     expect(Saks).toBeInTheDocument();
   });
 
-  test("Sjekk at stein er på plass", () => {
+  test('Sjekk at stein er på plass', () => {
     const { getByRole } = render(<App />);
-    const Stein = getByRole("button", { name: /stein/i });
+    const Stein = getByRole('button', { name: /stein/i });
     expect(Stein).toBeInTheDocument();
   });
 
-  test("Sjekk at papir er på plass", () => {
+  test('Sjekk at papir er på plass', () => {
     const { getByRole } = render(<App />);
-    const Papir = getByRole("button", { name: /papir/i });
+    const Papir = getByRole('button', { name: /papir/i });
     expect(Papir).toBeInTheDocument();
   });
 
-  test("Sjekk at poengtavle er på plass", () => {
+  test('Sjekk at poengtavle er på plass', () => {
     const { getByRole } = render(<App />);
-    const Poengtavle = getByRole("heading", { name: /poengtavle/i });
+    const Poengtavle = getByRole('heading', { name: /poengtavle/i });
     expect(Poengtavle).toBeInTheDocument();
   });
 
-  test("Sjekk at poengoversikt er på plass", () => {
+  test('Sjekk at poengoversikt er på plass', () => {
     const { getByRole } = render(<App />);
-    const Poengoversikt = getByRole("heading", { name: /poengoversikt/i });
+    const Poengoversikt = getByRole('heading', { name: /poengoversikt/i });
     expect(Poengoversikt).toBeInTheDocument();
   });
 });
