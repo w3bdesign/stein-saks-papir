9e4b7b5043b3919b2ce57e01f331b47ae68e3f92
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2022-03-21 20:27:56 +0100

    Format code with standardjs and prettier
    This commit fixes the style issues introduced in 2a2d4e8 according to the output
    from standardjs and prettier.
    
    Details: https://deepsource.io/gh/w3bdesign/stein-saks-papir/transform/0edd3f72-7d1a-4ed7-83a8-fede42a5ca09/

--- src/components/Pages/Index/Main.component.jsx
+++ src/components/Pages/Index/Main.component.jsx
@@ -1,12 +1,12 @@
-import React from 'react';
+import React from "react";
 
-import Col from 'react-bootstrap/Col';
-import Row from 'react-bootstrap/Row';
-import Card from 'react-bootstrap/Card';
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import Card from "react-bootstrap/Card";
 
-import Scoreboard from './Scoreboard.component';
-import MobileScoreboard from './MobileScoreBoard.component';
-import Game from './Game.component';
+import Scoreboard from "./Scoreboard.component";
+import MobileScoreboard from "./MobileScoreBoard.component";
+import Game from "./Game.component";
 
 /**
  * Hovedkomponenten som viser våpnene, resetknapp og poengtavle.
@@ -17,9 +17,7 @@ function Main() {
     <Row className="mt-2 text-center justify-content-center">
       <Col>
         <MobileScoreboard />
-        <Card
-          className="d-block mx-auto mt-4 text-center"
-        >
+        <Card className="d-block mx-auto mt-4 text-center">
           <Card.Header>
             <h6>Velg ditt våpen under for å starte spillet</h6>
           </Card.Header>
