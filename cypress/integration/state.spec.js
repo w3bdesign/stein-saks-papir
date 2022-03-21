bc3c03619c696eb512d6c025d12b2ce6089634ab
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2022-03-21 20:24:51 +0100

    Fjern debug url

--- cypress/integration/state.spec.js
+++ cypress/integration/state.spec.js
@@ -1,7 +1,7 @@
 describe('Test Redux store og se at state fungerer', () => {
   beforeEach(() => {
-    // cy.visit('/');
-    cy.visit('http://localhost:3000');
+    cy.visit('/');
+    // cy.visit('http://localhost:3000');
   });
 
   it('Sjekk at state inneholder default data', () => {
