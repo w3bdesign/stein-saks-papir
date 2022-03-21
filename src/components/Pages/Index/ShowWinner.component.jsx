6554faf25c7cf1b42cd263d5935edd7796f18b43
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2022-03-21 20:29:06 +0100

    React Bootstrap 2

--- src/components/Pages/Index/ShowWinner.component.jsx
+++ src/components/Pages/Index/ShowWinner.component.jsx
@@ -36,7 +36,6 @@ function ShowWinner({
       <Modal.Title>
         <h4 data-cy="winnermodaltext" className="text-center mt-2">
           Vinner:
-          {' '}
           {winner && winner.toString()}
         </h4>
       </Modal.Title>
