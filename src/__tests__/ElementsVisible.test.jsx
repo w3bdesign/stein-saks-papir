0c0f4bf7717066f684a930558229c5a74f943df2
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2022-03-21 20:28:23 +0100

    Eslint

--- src/__tests__/ElementsVisible.test.jsx
+++ src/__tests__/ElementsVisible.test.jsx
@@ -1,30 +1,30 @@
-import React from "react";
+import React from 'react';
 
-import { render } from "@testing-library/react";
+import { render } from '@testing-library/react';
 
-import App from "../App";
+import App from '../App';
 
 /**
  * Her tester vi at alle de nødvendige elementene i applikasjonen er synlige
  * som knapper, navbar osv
  */
 
-describe("Sjekk at alle nødvendige elementer er synlige", () => {
-  test("Sjekk at saks er synlig", () => {
+describe('Sjekk at alle nødvendige elementer er synlige', () => {
+  test('Sjekk at saks er synlig', () => {
     const { getByRole } = render(<App />);
-    const Saks = getByRole("button", { name: /saks/i });
+    const Saks = getByRole('button', { name: /saks/i });
     expect(Saks).toBeVisible();
   });
 
-  test("Sjekk at stein er synlig", () => {
+  test('Sjekk at stein er synlig', () => {
     const { getByRole } = render(<App />);
-    const Stein = getByRole("button", { name: /stein/i });
+    const Stein = getByRole('button', { name: /stein/i });
     expect(Stein).toBeVisible();
   });
 
-  test("Sjekk at papir er synlig", () => {
+  test('Sjekk at papir er synlig', () => {
     const { getByRole } = render(<App />);
-    const Papir = getByRole("button", { name: /papir/i });
+    const Papir = getByRole('button', { name: /papir/i });
     expect(Papir).toBeVisible();
   });
 });
