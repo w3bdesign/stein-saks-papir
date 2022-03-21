b64c4d0884d902ffc05cda4ccc5d14179184fbb8
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2022-03-21 20:23:39 +0100

    Legg til introtekst og våpen
    Vi trengte å legge til introtekst med forklaring. Vi la også til våpen i en separat komponent med SVG.

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import { render, screen } from '@testing-library/react';
 
-test('renders Stein Saks Papir', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/Stein Saks Papir/i);
-  expect(linkElement).toBeInTheDocument();
+import App from './App';
+import Header from './components/Header/Header.component';
+
+describe('Sjekk at alle nødvendige elementer er på plass i dokumentet', () => {
+  test('Sjekk at navbar er på plass', () => {
+    const { getByRole } = render(<Header />);
+    const Navbar = getByRole('heading', { name: /Stein Saks Papir/i });
+    expect(Navbar).toBeInTheDocument();
+  });
+
+  test('Sjekk at introtekst er på plass', () => {
+    const { getByRole } = render(<App />);
+    const Introtekst = getByRole('heading', {
+      name: /velkommen til stein, saks og papir/i,
+    });
+    expect(Introtekst).toBeInTheDocument();
+  });
 });
