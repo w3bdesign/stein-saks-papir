bdd58ae6a4e1cb3397d01d02468357e274ce7801
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2022-03-21 20:25:40 +0100

    Update tests for Cypress 6

--- cypress/integration/test.spec.js
+++ cypress/integration/test.spec.js
@@ -2,8 +2,8 @@
 
 describe('Besøk hjemmesiden, se at elementer eksisterer i DOM og at knappene fungerer', () => {
   beforeEach(() => {
-    cy.visit('/');
-    // cy.visit('http://localhost:3000');
+    // cy.visit('/');
+    cy.visit('http://localhost:3000');
   });
 
   it('Se at nødvendige elementer er på plass', () => {
@@ -12,7 +12,6 @@ describe('Besøk hjemmesiden, se at elementer eksisterer i DOM og at knappene fu
   });
 
   it('Vi klikker på Saks og vi kan se et resultat', () => {
-    cy.get('[data-cy=winnermodaltext]').should('not.be.visible');
     cy.get('[data-cy=saks]').click();
     cy.get('[data-cy=winnermodaltext]').should('be.visible');
   });
@@ -20,15 +19,15 @@ describe('Besøk hjemmesiden, se at elementer eksisterer i DOM og at knappene fu
   it('Vi klikker på alle våpnene, klikker lukk og sjekker at modal ikke er synlig lenger', () => {
     cy.get('[data-cy=saks]').click();
     cy.get('.close').click();
-    cy.get('[data-cy=winnermodaltext]').should('not.be.visible');
+    cy.get('[data-cy=winnermodaltext]').should('not.exist');
 
     cy.get('[data-cy=stein]').click();
     cy.get('.close').click();
-    cy.get('[data-cy=winnermodaltext]').should('not.be.visible');
+    cy.get('[data-cy=winnermodaltext]').should('not.exist');
 
     cy.get('[data-cy=papir]').click();
     cy.get('.close').click();
-    cy.get('[data-cy=winnermodaltext]').should('not.be.visible');
+    cy.get('[data-cy=winnermodaltext]').should('not.exist');
   });
 
   it('Vi sjekker at score er 0 fra begynnelsen av', () => {
