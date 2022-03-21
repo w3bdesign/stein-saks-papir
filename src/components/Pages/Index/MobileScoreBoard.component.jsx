142faf4085b3d73dcd3e7455d895c512bb4233bc
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2022-03-21 20:28:37 +0100

    Eslint

--- src/components/Pages/Index/MobileScoreBoard.component.jsx
+++ src/components/Pages/Index/MobileScoreBoard.component.jsx
@@ -16,8 +16,8 @@ function MobileScoreboard() {
           <h6>Poengoversikt</h6>
         </Card.Header>
         <Card.Body>
-          {Player &&
-            Player.map((player) => (
+          {Player
+            && Player.map((player) => (
               <div key={player.Name} className="m-2">
                 <Card.Header bg="secondary">{player.Name}</Card.Header>
                 <h5 className="font-weight-bold mt-2">{player.Score}</h5>
