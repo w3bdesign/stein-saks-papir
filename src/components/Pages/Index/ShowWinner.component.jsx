983066d11c2f6f05a4e98f1b742744d954edb802
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2022-03-21 20:23:48 +0100

    Vi viser nå hva motstanderen har valgt
    Det kan kanskje være en idè å vise det i form av en SVG?

--- src/components/Pages/Index/ShowWinner.component.jsx
+++ src/components/Pages/Index/ShowWinner.component.jsx
@@ -8,9 +8,15 @@ import Modal from 'react-bootstrap/Modal';
  * Viser hvem som har vunnet hver runde med en modal man kan lukke via show
  * @param {Boolean} show Skal vi vise modal eller ikke?
  * @param {Function} setShow Funksjon for å endre show til false (og lukke)
+ * @param {String} computerSelected Hva CPU har valgt
  * @param {String} winner Hvem som har vunnet
  */
-function ShowWinner({ showWinnerModal, setShowWinnerModal, winner }) {
+function ShowWinner({
+  showWinnerModal,
+  setShowWinnerModal,
+  computerSelected,
+  winner,
+}) {
   const handleClose = () => {
     setShowWinnerModal(false);
   };
@@ -23,13 +29,18 @@ function ShowWinner({ showWinnerModal, setShowWinnerModal, winner }) {
       centered
     >
       <Modal.Header closeButton />
-      <Modal.Title> </Modal.Title>
-      <Modal.Body>
+      <Modal.Title>
         <h3 className="text-center">
           Vinner:
           {' '}
-          {' '}
           {winner && winner.toString()}
+        </h3>
+      </Modal.Title>
+      <Modal.Body>
+        <h3 className="text-center">
+          Motstander valgte:
+          {' '}
+          {computerSelected}
         </h3>
       </Modal.Body>
     </Modal>
@@ -39,12 +50,14 @@ function ShowWinner({ showWinnerModal, setShowWinnerModal, winner }) {
 ShowWinner.defaultProps = {
   showWinnerModal: PropTypes.bool,
   setShowWinnerModal: PropTypes.func,
+  computerSelected: PropTypes.string,
   winner: PropTypes.string,
 };
 
 ShowWinner.propTypes = {
   showWinnerModal: PropTypes.bool,
   setShowWinnerModal: PropTypes.func,
+  computerSelected: PropTypes.string,
   winner: PropTypes.string,
 };
 
