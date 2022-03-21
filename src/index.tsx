c9c9a08c9a00a32c71de46b200772d152245afd5
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2022-03-21 20:23:41 +0100

    Slett Footer

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 
 import App from './App';
 import Header from './components/Layout/Header/Header.component';
-import Footer from './components/Layout/Footer/Footer.component';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -11,7 +10,6 @@ ReactDOM.render(
   <>
     <Header />
     <App />
-    <Footer />
   </>,
   document.getElementById('root'),
 );
