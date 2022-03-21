39ff84056a18f821ccce01db014a989d9636531d
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2022-03-21 20:28:22 +0100

    Eslint

--- src/components/Layout/Header/Header.component.jsx
+++ src/components/Layout/Header/Header.component.jsx
@@ -1,6 +1,6 @@
-import React from "react";
+import React from 'react';
 
-import Navbar from "react-bootstrap/Navbar";
+import Navbar from 'react-bootstrap/Navbar';
 
 function Header() {
   return (
