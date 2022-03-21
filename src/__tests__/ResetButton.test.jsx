8a0096e146211b0458bfb34320173b84a433e510
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2022-03-21 20:28:37 +0100

    Imports should be sorted alphabetically
    Expected 'none' syntax before 'single' syntax

--- src/__tests__/ResetButton.test.jsx
+++ src/__tests__/ResetButton.test.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
-import { render, screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
 import '@testing-library/jest-dom';
+import userEvent from '@testing-library/user-event';
+import { render, screen } from '@testing-library/react';
 import { describe, expect, test } from '@jest/globals';
 
 import App from '../App';
