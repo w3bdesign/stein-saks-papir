ea3277299d373eb7b36bc3ebe6b36bdcb5f5fa1a
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2022-03-21 20:23:39 +0100

    Add .gitmessage to write good commit messages
    Added a .gitmessage template that will be used as a base for all future commit messages after this one

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
 
+import Container from 'react-bootstrap/Container';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+
+
+
 function App() {
   return (
     <div className="App">
-      Stein Saks Papir
+      <Container fluid>
+        <Row>
+          <Col>Stein Saks Papir</Col>
+        </Row>
+      </Container>
+
     </div>
   );
 }
