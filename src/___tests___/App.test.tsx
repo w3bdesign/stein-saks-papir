dc8bb947cf283aec1d14491d1195c9d5fcfef1e1
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2022-03-21 20:23:40 +0100

    Fiks test av introtekst
    Etter at vi fjernet introtekst må vi også fjerne testen

--- src/___tests___/App.test.tsx
+++ src/___tests___/App.test.tsx
@@ -3,22 +3,14 @@ import React from 'react';
 // eslint-disable-next-line no-unused-vars
 import { render, screen } from '@testing-library/react';
 
-import App from './App';
-import Header from './components/Header/Header.component';
+import App from '../App';
+import Header from '../components/Header/Header.component';
 
 describe('Sjekk at alle nødvendige elementer er på plass i dokumentet', () => {
   test('Sjekk at navbar er på plass', () => {
     const { getByRole } = render(<Header />);
     const Navbar = getByRole('heading', { name: /Stein Saks Papir/i });
     expect(Navbar).toBeInTheDocument();
-  });
-
-  test('Sjekk at introtekst er på plass', () => {
-    const { getByRole } = render(<App />);
-    const Introtekst = getByRole('heading', {
-      name: /velkommen til stein, saks og papir/i,
-    });
-    expect(Introtekst).toBeInTheDocument();
   });
 
   test('Sjekk at saks er på plass', () => {
