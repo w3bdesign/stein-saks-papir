f051197a04b940767276a4320cf62301aedb57a7
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2022-11-13 23:24:53 +0100

    Update CheckFinalWinner.test.jsx

--- src/__tests__/CheckFinalWinner.test.jsx
+++ src/__tests__/CheckFinalWinner.test.jsx
@@ -21,8 +21,8 @@ describe('Velg et våpen og se at noen vinner til slutt', () => {
   });
 
   test('Velg et våpen flere ganger og se at vinner-boksen vises', async () => {
-    // Vi bruker en array og forEach for å klikke 10 ganger
-    const repeatClickTenTimes = Array.from({ length: 10 }, (_v, i) => i);
+    // Vi bruker en array og forEach for å klikke 15 ganger
+    const repeatClickTenTimes = Array.from({ length: 15 }, (_v, i) => i);
 
     render(<App />);
 
@@ -30,8 +30,9 @@ describe('Velg et våpen og se at noen vinner til slutt', () => {
       repeatClickTenTimes.forEach(() =>
         userEvent.click(screen.queryByRole('button', { name: /stein/i }))
       );
-      expect(screen.queryByRole('alert', { name: /gamewinner/i })).toBeVisible();
-      expect(screen.queryByRole('alert', { name: /gamewinner/i })).toHaveTextContent('Hurra!');
     });
+
+    expect(screen.queryByRole('alert', { name: /gamewinner/i })).toBeVisible();
+    expect(screen.queryByRole('alert', { name: /gamewinner/i })).toHaveTextContent('Hurra!');
   });
 });
