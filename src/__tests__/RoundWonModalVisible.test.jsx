0b8f07fa288991050611ab67978415ee2e3ce6f0
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2023-03-29 01:32:52 +0200

    Test ShowWinner

--- src/__tests__/RoundWonModalVisible.test.jsx
+++ src/__tests__/RoundWonModalVisible.test.jsx
@@ -8,6 +8,8 @@ import '@testing-library/jest-dom';
 
 import App from '../App';
 
+import ShowWinner from './ShowWinner';
+
 /**
  * Her tester vi at modal som dukker opp når vi har valgt et våpen fungerer som det skal
  * Vi klikker på hver enkelt knapp (stein, saks og papir) og kjører en test
@@ -55,5 +57,22 @@ describe('Sjekk at "Motstander valgte" er synlig når vi velger et våpen', () =
     await userEvent.click(screen.getByRole('button', { name: /papir/i }));
 
     expect(screen.queryByRole('button', { name: /close/i }).toBeNull);
+  });
+
+  test('Kaller setShowWinnerModal med false når modal er trykket på', () => {
+    const setShowWinnerModalMock = jest.fn();
+    const { getByTestId } = render(
+      <ShowWinner
+        showWinnerModal={true}
+        setShowWinnerModal={setShowWinnerModalMock}
+        computerSelected="Stein"
+        winner="Player"
+      />
+    );
+
+    const modal = getByTestId('modalId');
+    fireEvent.click(modal);
+
+    expect(setShowWinnerModalMock).toHaveBeenCalledWith(false);
   });
 });
