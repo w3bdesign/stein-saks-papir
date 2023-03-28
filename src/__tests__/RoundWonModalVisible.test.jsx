5855a5d9deedae10f1825847401fe0e0c3c22024
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2023-03-29 01:42:31 +0200

    Fiks tester

--- src/__tests__/RoundWonModalVisible.test.jsx
+++ src/__tests__/RoundWonModalVisible.test.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import { describe, expect, test } from '@jest/globals';
 
 import userEvent from '@testing-library/user-event';
