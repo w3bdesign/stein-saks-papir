f7582963228b0da03612c734daa162930863237b
Daniel Fjeldstad <45217974+w3bdesign@users.noreply.github.com>
2024-04-08 17:10:04 +0200

    Prettier

--- src/state/ScoreBoardStore.js
+++ src/state/ScoreBoardStore.js
@@ -12,20 +12,14 @@ const isDevelopmentEnv = process.env.NODE_ENV === 'development';
 const ScoreBoardStore = createStore({
   score: DEFAULT_SCORE,
   increaseScore: action((state, payload) => {
-
     const playerIndex = state.score.findIndex((element) => element.Name === payload);
     state.score[playerIndex].Score += 1;
-
-}),
+  }),
   reset: action((state) => {
-
     state.score.forEach((player) => {
-
       player.Score = 0;
-
-});
-
-}),
+    });
+  }),
   devTools: isDevelopmentEnv
 });
 
